253c2d2c5058d9fcc7463fef562fc8eb5102db1b
Mark Obermann <markobermann@Marks-MBP.fritz.box>
2018-06-08 16:28:17 +0200

    Test

--- Weather.py
+++ Weather.py
@@ -9,3 +9,4 @@ driver.get("https://www.weather-forecast.com/locations/"+city+"/forecasts/latest
 
 
 print(driver.find_elements_by_class_name("b-forecast__table-description-content")[0].text)
+#Test
